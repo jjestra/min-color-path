504216e26643a7e1b253dac591094057d94bb49b
Neeraj <neeraj@cs.ucsb.edu>
2019-02-08 14:39:32 -0800

    changes

--- batch-runs.sh
+++ batch-runs.sh
@@ -1,16 +1,17 @@
 #! /bin/sh -f
 
 # Running on more random instances
-#testcases="random layered unit-disk"
-testcases="unit-disk"
+testcases="random layered unit-disk"
 for t in $testcases
 do
 	echo "Running $t instances, STARTING TIME: `date`"
 	echo "-----------------------------------------------------"
-	timeout 4000s ./gen_graphs $t runLargeInstances
+	timeout 500s ./mcp $t noILP 
+	#timeout 2000s ./mcp $t noILP runLargeInstances
 	echo "-----------------------------------------------------"
 done
 
-echo "Running roadnet instances, STARTING TIME: `date`" 
-echo "-----------------------------------------------------"
-timeout 4000s ./gen_graphs roadnet noDijkstraBased
+# Takes about 2 hours for all tests to finish.
+#echo "Running roadnet instances, STARTING TIME: `date`" 
+#echo "-----------------------------------------------------"
+#timeout 10000s ./mcp roadnet noILP
